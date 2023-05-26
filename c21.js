a97a6db01d476461fc0c0813c52602dbabb2a79c
Osielprogramacion <96593392+Osielprogramacion@users.noreply.github.com>
2023-05-26 17:13:41 -0600

    Add files via upload

---
+++ c21.js
@@ -0,0 +1,46 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(900, 800);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//crear los cuerpos aquí.
+
+    var ball;
+	Engine.run(engine);
+    var ball options={
+	isStatic:false,
+	restitution:0.3,
+	friction:0,
+	density:1.2
+	}
+	groundObj.display();
+	groundobj=new ground(width/2,670,width,20);
+    leftSide = new ground(1100,600,20,126);
+
+}
+
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
